3bccdd988cb41b5b08b63d16ea1213513331e12e
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2024-03-26 09:13:52 -0400

    Move ancillary functions to helper.js file

---
+++ src/helpers.js
@@ -0,0 +1,27 @@
+const { row, column } = require('./conversions')
+
+function isOccupied(cell) {
+  return cell !== ' ' && cell !== 'X';
+}
+
+const checkCollisions = function checkForShipPlacementCollisions(board, x, y, orientation, ship) {
+    const shipSlot = [];
+    let colCheck = column(x);
+    let rowCheck = row(y);
+
+    if(orientation === 'vertical') {
+      for (let i = 1; i <= ship.readShipLength(); i++) {
+        shipSlot.push(board[rowCheck][colCheck]);
+        rowCheck++;
+      }
+    } else {
+      for (let i = 1; i <= ship.readShipLength(); i++) {
+        shipSlot.push(board[rowCheck][colCheck]);
+        colCheck++;
+      }
+    }
+
+    return shipSlot.find(isOccupied);
+  }
+
+module.exports = { checkCollisions, isOccupied }
