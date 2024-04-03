946b3458e6b76db7f4f0dc62e5329514fad14666
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2024-04-02 22:04:00 -0400

    Add function which returns possessive pronoun

--- src/ship.js
+++ src/ship.js
@@ -2,16 +2,13 @@ const Ship = (name, length) => {
     const shipLength = length;
     let hits = 0;
 
-    const readShipLength = () => shipLength;
-    const readHits = () => hits;
-
+    const getShipLength = () => shipLength;
     const isSunk = () => hits === shipLength;
-    
     const hit = () => { 
       if (!isSunk()) hits += 1;
     }
 
-    return { name, readShipLength, readHits, hit, isSunk }
+    return { name, getShipLength, hit, isSunk }
 }
 
 module.exports = Ship;
